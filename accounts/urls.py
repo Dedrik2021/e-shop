97a1e75a0c2b63f03f734139bfd7e54cbc023968
Oleg Grunski <oleggr578@gmail.com>
2024-03-04 15:18:15 +0100

    created dashboard template

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
-    path('activate/<uidb64>/<token>/', views.activate, name="activate")
+    path('activate/<uidb64>/<token>/', views.activate, name="activate"),
+    path('', views.dashboard,name="dashboard"),
+    path('dashboard/', views.dashboard,name="dashboard")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -65,7 +65,7 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             messages.success(request, 'You are logged in')
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, "Invalid login credentials")
             return redirect('login')
@@ -98,3 +98,8 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, "Invalid activation link!")
         return redirect('register')
+    
+    
+@login_required(login_url="login")
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
