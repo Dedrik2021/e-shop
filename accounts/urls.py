6de1831085f45503d856bb93c6dd23718441a6f3
Oleg Grunski <oleggr578@gmail.com>
2024-03-05 11:46:17 +0100

    created reset password validation

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('dashboard/', views.dashboard,name="dashboard"),
     path('forgot-password/', views.forgot_password, name="forgot_password"),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name="reset_password_validate"),
+    path('reset-password/', views.reset_password, name="reset_password")
 ]
