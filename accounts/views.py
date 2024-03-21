44f76dc68d54cdd4996cd0fe42e68ede9e51e71b
Oleg Grunski <oleggr578@gmail.com>
2024-03-21 09:35:58 +0100

    send order received email

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
